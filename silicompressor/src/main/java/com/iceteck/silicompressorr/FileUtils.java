761e8f5ee4543fd04777bb2ed9cebd9ea47b9901
吴可馨 <1053591833@qq.com>
2018-01-20 11:12:14 +0800

    删除

--- silicompressor/src/main/java/com/iceteck/silicompressorr/FileUtils.java
+++ silicompressor/src/main/java/com/iceteck/silicompressorr/FileUtils.java
@@ -275,7 +275,7 @@ public class FileUtils {
                         return Environment.getExternalStorageDirectory() + "/" + split[1];
                     }
 
-                    // TODO handle non-primary volumes
+
                 }
                 // DownloadsProvider
                 else if (isDownloadsDocument(uri)) {
