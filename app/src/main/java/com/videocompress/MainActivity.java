e0789d4b6f067bce2235be27a09113284d8820c9
吴可馨 <1053591833@qq.com>
2018-01-20 11:06:23 +0800

    视频压缩

---
+++ app/src/main/java/com/videocompress/MainActivity.java
@@ -0,0 +1,284 @@
+package com.videocompress;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.iceteck.silicompressorr.VideoCompress;
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.config.PictureMimeType;
+import com.luck.picture.lib.entity.LocalMedia;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    private Button localButton;
+    private Button compressButton;
+    private long startTime, endTime;
+    private String outputDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
+    private String inputDir="";
+    private TextView inputText;
+    private TextView outPutText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        localButton=findViewById(R.id.localButton);
+        compressButton=findViewById(R.id.compressButton);
+        localButton.setOnClickListener(this);
+        compressButton.setOnClickListener(this);
+        inputText=findViewById(R.id.inputText);
+        outPutText=findViewById(R.id.outPutText);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.localButton:
+                setPicture();
+                break;
+            case R.id.compressButton:
+               final String destPath = outputDir + File.separator + "VID_" + new SimpleDateFormat("yyyyMMdd_HHmmss", getLocale()).format(new Date()) + ".mp4";
+                outPutText.setText(destPath);
+                VideoCompress.compressVideoLow(inputDir, destPath, new VideoCompress.CompressListener() {
+                    @Override
+                    public void onStart() {
+                        startTime = System.currentTimeMillis();
+                        setTime(startTime,"开始时间");
+
+                    }
+
+                    @Override
+                    public void onSuccess() {
+                        endTime = System.currentTimeMillis();
+                        setTime(endTime,"结束时间");
+                        Log.i(TAG,"压缩后大小 = "+getFileSize(destPath));
+                        openFile(new File(destPath));
+                    }
+
+                    @Override
+                    public void onFail() {
+                        endTime = System.currentTimeMillis();
+                        setTime(endTime,"失败时间");
+                    }
+
+                    @Override
+                    public void onProgress(float percent) {
+                        Log.i(TAG,String.valueOf(percent) + "%");
+                    }
+                });
+                break;
+        }
+    }
+
+    private void setPicture(){
+        // 进入相册 以下是例子：用不到的api可以不写
+        PictureSelector.create(MainActivity.this)
+                .openGallery(PictureMimeType.ofVideo())//全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
+                .imageSpanCount(4)// 每行显示个数 int
+                .selectionMode(PictureConfig.SINGLE)// 多选 or 单选 PictureConfig.MULTIPLE or PictureConfig.SINGLE
+                .previewVideo(true)// 是否可预览视频 true or false
+                .enablePreviewAudio(true) // 是否可播放音频 true or false
+                .isCamera(false)// 是否显示拍照按钮 true or false
+                .openClickSound(false)// 是否开启点击声音 true or false
+                .videoQuality(1)// 视频录制质量 0 or 1 int
+                .videoMaxSecond(60)// 显示多少秒以内的视频or音频也可适用 int
+                .videoMinSecond(10)// 显示多少秒以内的视频or音频也可适用 int
+                .recordVideoSecond(60)//视频秒数录制 默认60s int
+                .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode==RESULT_OK){
+            switch (requestCode){
+                case PictureConfig.CHOOSE_REQUEST:
+                    List<LocalMedia> selectList = PictureSelector.obtainMultipleResult(data);
+                    for (int i = 0; i <selectList.size() ; i++) {
+                        inputDir=selectList.get(i).getPath();
+                        inputText.setText(inputDir);
+                        Log.i(TAG,"压缩前大小 = "+getFileSize(inputDir));
+                    }
+                    break;
+            }
+        }
+    }
+
+
+    private Locale getLocale() {
+        Configuration config = getResources().getConfiguration();
+        Locale sysLocale = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            sysLocale = getSystemLocale(config);
+        } else {
+            sysLocale = getSystemLocaleLegacy(config);
+        }
+
+        return sysLocale;
+    }
+
+    @SuppressWarnings("deprecation")
+    public static Locale getSystemLocaleLegacy(Configuration config){
+        return config.locale;
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    public static Locale getSystemLocale(Configuration config){
+        return config.getLocales().get(0);
+    }
+
+    private void setTime(Long time,String type){
+        SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date=new Date(time);
+        Log.i(TAG,type+" = "+dateFormat.format(date));
+    }
+
+
+    private void openFile(File file) {
+        try {
+            Intent intent = new Intent();
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            //设置intent的Action属性
+            intent.setAction(Intent.ACTION_VIEW);
+            //获取文件file的MIME类型
+            String type = getMIMEType(file);
+            //设置intent的data和Type属性。
+            intent.setDataAndType(/*uri*/Uri.fromFile(file), type);
+            //跳转
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, "找不到文件", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    /**
+     * 根据文件后缀名获得对应的MIME类型。
+     *
+     * @param file
+     */
+    private String getMIMEType(File file) {
+
+        String type = "*/*";
+        String fName = file.getName();
+        //获取后缀名前的分隔符"."在fName中的位置。
+        int dotIndex = fName.lastIndexOf(".");
+        if (dotIndex < 0) {
+            return type;
+        }
+        /* 获取文件的后缀名*/
+        String end = fName.substring(dotIndex, fName.length()).toLowerCase();
+        if (end == "") return type;
+        //在MIME和文件类型的匹配表中找到对应的MIME类型。
+        for (int i = 0; i < MIME_MapTable.length; i++) {
+            if (end.equals(MIME_MapTable[i][0]))
+                type = MIME_MapTable[i][1];
+        }
+        return type;
+    }
+
+    private final String[][] MIME_MapTable = {
+            //{后缀名，MIME类型}
+            {".3gp", "video/3gpp"},
+            {".apk", "application/vnd.android.package-archive"},
+            {".asf", "video/x-ms-asf"},
+            {".avi", "video/x-msvideo"},
+            {".bin", "application/octet-stream"},
+            {".bmp", "image/bmp"},
+            {".c", "text/plain"},
+            {".class", "application/octet-stream"},
+            {".conf", "text/plain"},
+            {".cpp", "text/plain"},
+            {".doc", "application/msword"},
+            {".docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
+            {".xls", "application/vnd.ms-excel"},
+            {".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+            {".exe", "application/octet-stream"},
+            {".gif", "image/gif"},
+            {".gtar", "application/x-gtar"},
+            {".gz", "application/x-gzip"},
+            {".h", "text/plain"},
+            {".htm", "text/html"},
+            {".html", "text/html"},
+            {".jar", "application/java-archive"},
+            {".java", "text/plain"},
+            {".jpeg", "image/jpeg"},
+            {".jpg", "image/jpeg"},
+            {".js", "application/x-javascript"},
+            {".log", "text/plain"},
+            {".m3u", "audio/x-mpegurl"},
+            {".m4a", "audio/mp4a-latm"},
+            {".m4b", "audio/mp4a-latm"},
+            {".m4p", "audio/mp4a-latm"},
+            {".m4u", "video/vnd.mpegurl"},
+            {".m4v", "video/x-m4v"},
+            {".mov", "video/quicktime"},
+            {".mp2", "audio/x-mpeg"},
+            {".mp3", "audio/x-mpeg"},
+            {".mp4", "video/mp4"},
+            {".mpc", "application/vnd.mpohun.certificate"},
+            {".mpe", "video/mpeg"},
+            {".mpeg", "video/mpeg"},
+            {".mpg", "video/mpeg"},
+            {".mpg4", "video/mp4"},
+            {".mpga", "audio/mpeg"},
+            {".msg", "application/vnd.ms-outlook"},
+            {".ogg", "audio/ogg"},
+            {".pdf", "application/pdf"},
+            {".png", "image/png"},
+            {".pps", "application/vnd.ms-powerpoint"},
+            {".ppt", "application/vnd.ms-powerpoint"},
+            {".pptx", "application/vnd.openxmlformats-officedocument.presentationml.presentation"},
+            {".prop", "text/plain"},
+            {".rc", "text/plain"},
+            {".rmvb", "audio/x-pn-realaudio"},
+            {".rtf", "application/rtf"},
+            {".sh", "text/plain"},
+            {".tar", "application/x-tar"},
+            {".tgz", "application/x-compressed"},
+            {".txt", "text/plain"},
+            {".wav", "audio/x-wav"},
+            {".wma", "audio/x-ms-wma"},
+            {".wmv", "audio/x-ms-wmv"},
+            {".wps", "application/vnd.ms-works"},
+            {".xml", "text/plain"},
+            {".z", "application/x-compress"},
+            {".zip", "application/x-zip-compressed"},
+            {"", "*/*"}
+    };
+
+
+
+    private String getFileSize(String path) {
+        File f = new File(path);
+        if (!f.exists()) {
+            return "0 MB";
+        } else {
+            long size = f.length();
+            return (size / 1024f) / 1024f + "MB";
+        }
+    }
+}
